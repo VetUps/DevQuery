e70cb9c305aec6d2d9acdacd364d5716ba031fba
VetUps <sercherufa@gmail.com>
2026-05-04 11:26:32 +0500

    feat: добавить QuestionTagInput с chips и autocomplete

--- src/backend/config/settings/settings.py
+++ src/backend/config/settings/settings.py
@@ -90,16 +90,24 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # База данных
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DATABASE_NAME'),
-        'USER': os.getenv('DATABASE_USER'),
-        'PASSWORD': os.getenv('DATABASE_USER_PASSWORD'),
-        'HOST': os.getenv('DATABASE_HOST'),
-        'PORT': os.getenv('DATABASE_PORT'),
+if os.getenv('DJANGO_TEST_SQLITE', '').strip().lower() in {'1', 'true', 'yes', 'on'}:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'test.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.getenv('DATABASE_NAME'),
+            'USER': os.getenv('DATABASE_USER'),
+            'PASSWORD': os.getenv('DATABASE_USER_PASSWORD'),
+            'HOST': os.getenv('DATABASE_HOST'),
+            'PORT': os.getenv('DATABASE_PORT'),
+        }
+    }
 
 
 # Password validation
